634d53bb5f3f63a675bc28058fa58c96b4e49fc5
Khauri <kmcclain@email.wm.edu>
2016-03-11 11:49:46 -0500

    API tests and bug fixes

--- Viano2.js
+++ Viano2.js
@@ -5,12 +5,23 @@
 
 var Viano = (function() {
 
-    var notemap = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"];
+    var notemap = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
     /*
      * Viano_Keys
      */
-    var Key = function(p) {
-
+    var Key = function(note, p) {
+        var p = p || {};
+        this.note = note;
+        this.width = p.width;
+        this.height = p.height;
+        this.x = p.x;
+        this.y = p.y;
+        this.color = p.color;
+        this.highlight = p.highlight;
+        this.outline = p.outline;
+        this.index = p.index || undefined;
+        this.customRender = p.render || false;
+        this.trigger_cache = [];
     }
     Key.prototype = {
         /*
@@ -25,6 +36,8 @@ var Viano = (function() {
         state: 0,
         /*
          * Set the key's state
+         * trigger all personal key trigger events
+         * bubble up to VianoObject trigger events
          */
         trigger: function(value) {
             // if value is undefined then toggle the current state to either an "on" or "off" value.
@@ -52,24 +65,40 @@ var Viano = (function() {
 
             previous_state = this.state;
             this.state = value;
-            // Call all the functions
+            // Call all the onTrigger functions and pass it this
             var s = {
                 key: this,
+                note: this.note,
+                index: this.index,
                 active: this.active,
                 state: this.state,
-                previous_state: previous_state
+                previous_state: previous_state,
+                continueChain: true,
+            }
+            for (var i = 0, len = this.trigger_cache.length; i < len; i++) {
+                this.trigger_cache[i].call(this, s);
             }
         },
         /*
          * Set an action when this particular key is triggered
          */
-        onTrigger: function() {
+        onTrigger: function(fn) {
+            if (typeof fn == "function") {
+                this.trigger_cache.push(fn);
+            } else {
+                //error
+            }
+        },
+        //sets the state without triggering any events
+        setState: function() {
 
         },
-        _render: function() {
+        /*
+         * Render the keys based on
+         */
+        render: function() {
 
         }
-
     }
     /*
      * VianoObject
@@ -90,12 +119,14 @@ var Viano = (function() {
             keys: p.keys || 12,
             // An auto-generation scheme. Defaults to a regular key layout.
             // Any key with a $ (flat) or # (sharp) at the end becomes a secondary key
-            scheme: notemap,
+            scheme: p.notemap || notemap,
+            render: p.render || true,
             primary: {
                 color: p.primary.color || "white",
                 outline: p.primary.outline || "gray",
                 highlight: p.primary.highlight || undefined,
                 width: p.primary.width || undefined,
+                render: p.primary.render || undefined
             },
             secondary: {
                 color: "black",
@@ -103,24 +134,50 @@ var Viano = (function() {
                 highlight: undefined,
                 width: undefined,
                 height: undefined,
+                render: p.secondary.render || undefined
             }
         }
         this.generate();
     };
     VianoObject.prototype = {
-        view: undefined,
+        /* 
+         * The canvas and its associated ctx
+         */
+        view: {
+            canvas: undefined,
+            ctx: undefined
+        },
 
         keys: [],
-
-        getKey: function(index) {
-
-        },
-        forEachKey: function(fn) {
-            if (typeof fn == "function") {
-
+        /*
+         * Get a key using a 0-based index
+         * determine whether or not that indexing is circular
+         */
+        getKey: function(value, circular) {
+            switch (typeof value) {
+                case "number":
+                    return (circular === true) ? this.keys[value % this.keys.length] : this.keys[value];
+                    break;
+                case "string":
+                    //search for each key
+                    break;
+                case "object":
+                    //iterate through array
+                    break;
+                default:
+                    //error
+                    break;
             }
         },
-        //adds a key to the end
+        forEachKey: function(fn, start_index) {
+            var si = start_index || 0;
+            if (typeof fn == "function") {
+                for (var i = si, len = this.keys.length; i < len; i++) {
+                    fn.call(this,this.keys[i]);
+                }
+            }
+        },
+        //adds a key to the end or a specific index
         addKey: function(index) {
 
         },
@@ -130,26 +187,56 @@ var Viano = (function() {
         //create the canvas and such
         generate: function(start_index) {
             var scheme = this.settings.scheme,
-                len = scheme.length - 1,
+                len = scheme.length,
                 i = start_index || 0,
                 keys = this.settings.keys + i,
-                key;
+                key, isSecondary;
+            //send a nice warning if about to generate a lot of keys
             if (keys >= 512) {
                 console.warn("What, really? You need " + keys + "? Okay, but don't blame me if your browser crashes...")
             }
             while (i < keys) {
                 key = scheme[i % len];
 
+                isSecondary = (key[key.length - 1] == "#" || key[key.length - 1] == "$"); //could be more robust in checking
+
+                if (isSecondary) {
+                    key = new Key(key, this.settings.secondary);
+                } else {
+                    key = new Key(key, this.settings.primary);
+
+                }
+                key.index = this.keys.push(key) - 1;
                 i++;
             }
+            //generate the canvas
             this.view = generate_canvas.call(this);
+            //render
+            if (this.settings.render === true) {
+                this.render();
+            }
+        },
+        render: function() {
+            if (this.view.canvas && this.view.ctx) {
+                //Draw all the keys to the canvas
+                //this.forEachKey(function(key) {
+
+                //});
+                //Rotate/resize canvas if necessary
+
+            } else {
+                //error
+            }
         },
         /*
          * toString method returns a canvas
          */
+        onTrigger: function(fn) {
+
+        },
         toString: function ts() {
-            return this.view;
-        }
+            return this.view.canvas;
+        },
     };
     /*
      * Helpers
@@ -190,10 +277,13 @@ var Viano = (function() {
     //creates a canvas and such
     //call, apply, or bind
     function generate_canvas() {
-        console.log(this);
+        if (!document) return false; //error;
         var c = document.createElement('canvas');
-        return c;
+        c.innerHTML = "Sorry HTML5 Canvas Not Supported In This Browser";
+        return {
+            canvas: c,
+            ctx: c.getContext('2d')
+        };
     }
     return VianoObject;
 })();
-Viano();
