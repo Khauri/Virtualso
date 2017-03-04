39599384257442cda97b71196dba0a42cd85ecb4
Khauri McClain <Khauri McClain>
2017-03-04 11:59:04 -0500

    refactoring

--- Virtualso/Instrument.js
+++ Virtualso/Instrument.js
@@ -4,26 +4,32 @@
  * All 
  */
 class Instrument{
-    constructor(opts){
-        this.active = true;
-        this.scheme = this.constructor.defaultNotemap;
-        this.keymap = this.constructor.defaultKeymap;
-        this.rotation = 0;
-        /*
-         * Set top, left, width, and height of instrument
-         * useful for positioning inside another view and also
-         * keeping
-         */
-        this.height = 250;
-        this.width = 500;
-        this.top = 0;
-        this.left = 0;
-        /**
-         * [left, top, right, buttom] padding of container
-         * Set to 1 to fix a particular bug
-         */
-        this.pad = [1,1,1,1];
+    constructor(...opts){
+        Instrument.__merge(this, 
+        {
+            active : true,
 
+            scheme : this.constructor.defaultNotemap,
+            keymap : this.constructor.defaultKeymap,
+
+            rotation : 0,
+
+            /*
+            * Set top, left, width, and height of instrument
+            * useful for positioning inside another view and also
+            * keeping
+            */
+            width : 500,
+            height : 250,
+            top : 0,
+            left : 0,
+            /**
+             * [left, top, right, buttom] padding of container
+             * Set to 1 to fix a particular bug
+             */
+            pad : [1,1,1,1]
+        },
+        ...opts);
         this.__events = { 
             "hover": [], 
             "keydown": [],
@@ -58,6 +64,8 @@ class Instrument{
      * Calls the events
      */
     callEvents(data,...types){
+        if(!this.active) 
+            return false;
         var type;
         while(types.length > 0){
             // TODO: string check
@@ -68,6 +76,7 @@ class Instrument{
                 })
             }
         }
+        return true;
     }
     /**
      * The function called before the instrument is rendered
@@ -120,31 +129,88 @@ class Instrument{
         this._view = v;
         this._view._context = v.getContext('2d');
     }
+
     /* Static Methods */
     static get defaultNotemap(){
         return ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
     }
+
     /* Private Methods */
+
     /**
-     * Deep merge the properties of this object and any number of objects passed as arguments
-     * Similar to underscore's extend
-     * Infinite loops are possible...
+     * Deep merge the properties of this object and any number of objects passed as arguments.
+     * Similar to Object.Assign, but goes deeper. 
+     * Infinite loops (and recursive errors) are possible so watch out...
      */
-    __merge( ...sources ){
+    static __merge( target, ...sources ){
         var source;
         while(sources.length){
-            source = source.shift();
-            if (!!source && source.constructor == Object){
-                this.__merge(source);
-            }else{
-
+            source = sources.shift();
+            if (!!source && source.constructor == Object){ //merge objects
+                for(let prop in source){
+                    if(target[prop] && target[prop].constructor == Object && source[prop].constructor == Object){
+                        Instrument.__merge(target[prop], source[prop]); // deep merge
+                    }else{
+                        target[prop] = source[prop];
+                    }
+                }
             }
         }
     }
     /**
      * Handles any events called upon the view
      */
-    __viewEventHandler(){
+    __viewEventHandler(e){
+        // calculate position 
+        var rect = e.target.getBoundingClientRect();
+        var x = e.clientX - rect.left - this.top,
+            y = e.clientY - rect.top -  this.left;
+        // now transform mouse position by rotation
+        // 
+        switch(e.type){
+            default:
+                this.callEvents(
+                    {   
+                        type : e.type,
+                        x:x, 
+                        y:y
+                    }, e.type);
+                break;
+        }
+    }
+}
+/**
+ * A playable component of an instrument
+ * Examples include Guitar Strings, Piano Keys, Drumpads, etc...
+ */
+class Playable{
+    constructor(parent, ...opts){
+        Instrument.__merge(this, {
+            parent : parent,
+            instrumentName : parent.constructor.name,
+            note : 'C',
+            octave : 4,
+            width : 0,
+            height : 0,
+            top : 0,
+            left : 0,
+            state : 0
+        }, ...opts);
+    }
 
+    onTrigger(){
+
+    }
+
+    render(){
+
+    }
+    /**
+     * Checks if a point (x, y) intersects this playable.
+     * *Note: does not automatically transform x and y by the rotation or position!*
+     */
+    _isInIntersection(x, y){
+        return (x >= this.left && x <= this.left + this.width) ? 
+                    (y >= this.top && y <= this.top + this.height) : false;
     }
 }
--- Virtualso/Viano/Viano.js
+++ Virtualso/Viano/Viano.js
@@ -8,34 +8,34 @@
  * @author AnotherBlacKid
  */
 class Viano extends Instrument{
-    constructor( opts ){
-        super();
-        Object.assign(this,
-            {
-                range: ["C", 12],
-                noteNames : false,
-                white: {
-                    width : null, // auto-generate
-                    height : null,
-                    fill : "#fff",
-                    stroke : "#222",
-                    render : null,
-                },
-                black: {
-                    width : null, // auto-generate
-                    height : null,
-                    fill : "#333",
-                    stroke : "#222",
-                    render : null,
-                }
-            }, opts);
-        // These values are set after the user's values so that they can't be overwritten
-        this.keys = [];
-        this.data = {
-            accidentals : 0,
-            naturals: 0
-        };
-        this.isInBrowser = !!(window && document);
+    constructor(...opts){
+        super({ // overwritable
+            range: ["C", 12],
+            noteNames : false,
+            white: {
+                width : null, // auto-generate
+                height : null,
+                fill : "#fff",
+                stroke : "#222",
+                render : null,
+            },
+            black: {
+                width : null, // auto-generate
+                height : null,
+                fill : "#333",
+                stroke : "#222",
+                render : null,
+            }
+        },
+        ...opts, //user's overwritable options
+        { // not overwrittable
+            keys : [],
+            data : {
+                accidentals : 0,
+                naturals : 0
+            },
+            isInBrowser : !!(window && document)
+        });
         // set up the view and such
         this._init();
         // generate the viano
@@ -96,17 +96,37 @@ class Viano extends Instrument{
             height = (this.white.height || this.height - this.pad[1] - this.pad[3]);
 
         this.forEachKey(function(key, index){
-            key.box = [x, y, width, height];
+            key.top = y;
             if(key.accidental){
                 ctx.globalCompositeOperation = "source-over";
-                key.render(ctx, x - width/4, y, width/2, height/1.5);
-                //x += width
+                key.left = x - width/4;
+                key.width = width/2;
+                key.height = height/1.5;
             }else{
                 ctx.globalCompositeOperation = "destination-over";
-                key.render(ctx, x, y, width, height);
+                key.left = x;
+                key.width = width;
+                key.height = height;
                 x += width;
             }
+            key.render(ctx);
         });
+    }
+    /**
+     * Returns the note at a particular position
+     */
+    getKeyAtPosition(x, y){
+        // if the viano is rotated "unrotate" it by rotating x, y around middle
+        var key;
+        for(var i = 0; i < this.keys.length; i++){
+            key = this.keys[i];
+            if(key._isInIntersection(x,y))
+                return key;
+        }
+    }
+
+    mousedown(e){
+
     }
     /**
      * This method is called when any key is triggered on the keyboard
@@ -133,19 +153,12 @@ class Viano extends Instrument{
         this.view.width = this.width; 
         this.view.height = this.height;
     }
-    /**
-     * Draws all the keys
-     */
-    _draw(){
-        this.forEachKey(function(key, index){
-            key._draw();
-        });
-    }
 
     /**
      * Generate all the keys using the range
      */
     _generate(){
+        // generator range parser
         var start = this.range[0].toUpperCase().replace('S',"#").replace(/\d/gi, ""),
             octave = parseInt(this.range[0].replace(/[^\d]/gi, "") , 10) || 0,
             keysToGenerate;
@@ -163,14 +176,16 @@ class Viano extends Instrument{
                     keysToGenerate = 1 + this.scheme.indexOf(end) - this.scheme.indexOf(start) + (octave + endOctave)*this.scheme.length;
                 break;
         }
-
-        var key, note;
+        // generator
+        var key, note, isAccidental;
         for(var i = 0, index = this.scheme.indexOf(start); i < keysToGenerate; i++, index = (index + 1) % this.scheme.length){
             note = this.scheme[index];
+            isAccidental = note.indexOf('#') > -1;
             key = new Key(this, {
                 'note' : note,
-                'accidental' : note.indexOf('#') > -1,
-                'octave' : octave
+                'accidental' : isAccidental,
+                'octave' : octave,
+                'options' : isAccidental ? this.black : this.white
             })
             // track accidentals and naturals
             if(key.accidental) this.data.accidentals ++;
@@ -191,56 +206,46 @@ class Viano extends Instrument{
     }
 }
 
-class Key{
-    constructor(viano, opts){
-        Object.assign(this, {
-            viano : viano,
-            note : "C",
-            type : 0, // 0 for white, 1 for black
-            /**
-             * Top, Left, Width, Height
-             */
-            box : [],
-            pad : [],
-            customRender : null,
-            outline: "#000",
-            color : "",
-            active_color : "",
-            state : 0
-        }, opts);
+class Key extends Playable{
+    constructor(viano, ...opts){
+        super(viano, {
+            options: {
+                stroke : "#000",
+                fill : "#fff",
+                activeFill : "#aaa",
+                width : 0,
+                height : 0,
+                render : null,
+            }
+        }, ...opts);
     }
     /**
-     * 
+     * Changes the state of the key limited
+     * Range [0, 1]
      */
     trigger( val ){
+       this.state = Math.min(1, Math.max(0, val));
+       if(this.state === NaN) this.state = 1;
 
+       this.parent.render(); // remove later and orce user to call render themselves
+       return this;
     }
     /**
      * Renders the key
      * TODO: svg fallback(?)
      */
-    render(ctx, top, left, width, height){
-        if(this.accidental){
-            ctx.strokeStyle = this.viano.black.stroke;
-            ctx.fillStyle = this.viano.black.fill;
+    render(ctx){
+        if(this.state <= 0){
+            ctx.strokeStyle = this.options.stroke;
+            ctx.fillStyle = this.options.fill;
         }else{
-            ctx.strokeStyle = this.viano.white.stroke;
-            ctx.fillStyle = this.viano.white.fill;
+            ctx.strokeStyle = this.options.activeStroke;
+            ctx.fillStyle = this.options.activeFill;
         }
-        ctx.fillRect(top, left, width, height);
-        ctx.strokeRect(top, left, width, height);
-    }
-    /**
-     * Sets the state to a value between 0 and 1
-     */
-    /*set state( val ){
-        this._state = Math.min(1, Math.max(0, val));
-    }*/
-
-    /**
-     * Checks if a point (x, y) intersects this key
-     */
-    _isInIntersection(x, y){
-        // transform the hitbox by the rotaton
+        
+        ctx.fillRect(this.left, this.top, this.width, this.height);
+        ctx.strokeRect(this.left, this.top, this.width, this.height);
     }
 }
+
+if (typeof module != "undefined" && module.exports) module.exports = Viano;
